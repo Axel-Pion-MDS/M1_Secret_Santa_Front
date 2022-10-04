6c4704d5cdf927b1e83343fee76f673cee3124b0
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2022-10-04 22:05:33 +0200

    feat: inputs

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,12 +3,24 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
+import InputText from '../inputs/inputText/inputText';
+
 function Home() {
     const dispatch = useDispatch();
 
     return (
         <div className="content" id="Home">
-            Home
+
+            <div className="spacer" style={{padding:'20px'}}></div>
+
+            <InputText
+                id="test"
+                minLength="3" 
+                maxLength="24"
+                label="Name"
+                placeholder="Your name"
+            />
+
         </div>
     );
 }
